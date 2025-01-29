20e1828f29303bfe8ce4e4d9decba3a2b86004dd
NoahRossetti <noahdavidrossetti@gmail.com>
2025-01-29 18:45:59 -0500

    Update vm.c

--- vm.c
+++ vm.c
@@ -128,6 +128,36 @@ while(fscanf(inputfile, "%d", &placeholder )){
     //Arithmentic operations will go in case 0 and will vary based on l
     case 0;
     switch(ir[1]){
+      case 1;
+
+      break;
+      case 2;
+
+      break;
+      case 3;
+
+      break;
+      case 4;
+
+      break;
+      case 5;
+
+      break;
+      case 6;
+
+      break;
+      case 7;
+
+      break;
+      case 8;
+
+      break;
+      case 9;
+
+      break;
+      case 10;
+
+      break;
     }
     break;
 
